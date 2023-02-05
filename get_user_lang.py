53f08b16c59d0f22c67d83c699968bf405df492f
peabnuts <30945867+peabnuts@users.noreply.github.com>
2023-02-04 18:42:39 -0800

    Update get_user_lang.py

--- get_user_lang.py
+++ get_user_lang.py
@@ -1,5 +1,6 @@
 import requests
 
+
 # Expects a string username
 def get_user_languages(username):
     
@@ -11,15 +12,29 @@ def get_user_languages(username):
     repos = response.json()
     
     # extract languages
+    
     user_languages = set()
+    user_lan = []
+    occ = []
+
     
     for i in range(len(repos)):
         j = repos[i]['language']
-        if j:
+       
+        if j not in user_lan and j:
+            user_lan.insert(i, j)
+            occ.insert(i, 1)
+            
+        elif j:
             user_languages.add(j)
+            ind = user_lan.index(j)
+            occ[ind] = occ[ind] + 1
     
-    
-    lang_str = ', '.join(user_languages)
+    occ_sorted = sorted(range(len(occ)),key=lambda user_lan:occ[user_lan],reverse=True)
+    langs_sorted = [user_lan[i] for i in occ_sorted ]
+
+
+    lang_str = ', '.join(langs_sorted)
     return lang_str
 
-    
+
